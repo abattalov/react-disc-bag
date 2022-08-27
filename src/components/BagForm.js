055a4c0da16c0da9ec0058e21aede8fbd858fbb8
abattalov <abattalov14@gmail.com>
2022-08-27 09:03:55 -0500

    added controlled form for bag creation

--- src/components/BagForm.js
+++ src/components/BagForm.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 function BagForm(){
+
+    const [state, setState] = React.useState({
+        name: ''
+    })
+
+    const handleChange = (e) => {
+        setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(state)
+    }
+
     return(
         <div>
             <h2>New Bag</h2>
-            <form className='bag-form'>
+            <form className='bag-form' onSubmit={handleSubmit}>
+                <label htmlFor="bagName">Bag Name</label>
                 <input
                     type='text'
                     placeholder='Bag Name'
-                    name="bagName"
-                    // value=''
+                    id="bagName"
+                    name="name"
+                    value={state.name}
+                    onChange={handleChange}
                 />
                 <button>Add Bag</button>
             </form>
