b0ff1fa832a57aa9ffe3705b8cd66247604577c4
abattalov <abattalov14@gmail.com>
2022-08-27 22:17:09 -0500

    added show pages for each bag and it's contents

--- src/components/BagCard.js
+++ src/components/BagCard.js
@@ -1,11 +1,58 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {useParams} from 'react-router-dom'
 
-function BagCard() {
+function BagCard(props) {
+
+  let params = useParams();
+
+  // props.bags.forEach(element => {
+  //   if(element.id === parseInt(params.id)){
+  //     console.log(element)
+  //   }
+  // });
+
+  const bag = props.bags.find(bag => bag.id === parseInt(params.id))
+
   return (
     <div>
-        <h1>yooo</h1>
+        <h2>{bag.name}</h2>
+        <table className="disc-table" >
+          <thead>
+                <tr>
+                    <th>Disc Name</th>
+                    <th>Brand</th>
+                    <th>Speed</th>
+                    <th>Glide</th>
+                    <th>Turn</th>
+                    <th>Fade</th>
+                    <th>Plastic</th>
+                    <th>Weight</th>
+                </tr>
+          </thead>
+          {bag.discs.map(disc => (
+          <tbody key={disc.id}>
+            <tr>
+              <td>{disc.name}</td>
+              <td>{disc.brand}</td>
+              <td>{disc.speed}</td>
+              <td>{disc.glide}</td>
+              <td>{disc.turn}</td>
+              <td>{disc.fade}</td> 
+              <td>{disc.plastic}</td>
+              <td>{disc.weight}</td>
+            </tr>
+          </tbody>
+          ))}
+        </table>
     </div>
   )
 }
 
-export default BagCard
+const mapStateToProps = state => {
+    return{
+      bags: state.bags
+    }
+}
+
+export default connect(mapStateToProps)(BagCard)
